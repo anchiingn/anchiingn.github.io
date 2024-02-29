06d6774e85cc92bbde73c178b14c86afb81a2098
Anchi <anchi.n248@gmail.com>
2024-02-28 20:14:29 -0600

    projects

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.to('.one', {
     duration: 20,
-    opacity:0,
+    opacity: 0,
     scrollTrigger: {
         trigger: '.one',
         start: '65% center',
-        end: 'bottom center', 
+        end: 'bottom center',
+        scrub: true,
+        markers: false
+    }
+})
+
+gsap.to('.two', {
+    duration: 20,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: '.two',
+        start: '65% center',
+        end: 'bottom center',
         scrub: true,
         markers: false
     }
@@ -16,12 +28,96 @@ gsap.to('.one', {
 
 gsap.to('#project_menu', {
     duration: 5,
-    opacity:0,
+    opacity: 0,
     scrollTrigger: {
         trigger: '#project_menu',
-        start: 'top 30%',
-        end: 'bottom 10%', 
+        start: 'top top',
+        end: 'bottom 5%',
         scrub: true,
-        markers: true
+        // markers: true
     }
 })
+
+// gsap.utils.toArray('.project_num').forEach((element) => {
+//     gsap.timeline({
+//         scrollTrigger: {
+//             trigger: element,
+//             scrub: 1,
+//         }
+//     })
+//         .to(element, {
+//             stagger: 1,
+//             y: -35,
+//             scrollTrigger: {
+//                 trigger: element,
+//                 start: '-10% 30%',
+//                 end: '15% 35%',
+//                 scrub: 2,
+//                 // markers: true
+//             }
+//         });
+// });
+
+// gsap.utils.toArray('.p-name').forEach((element) => {
+//     gsap.timeline({
+//         scrollTrigger: {
+//             trigger: element,
+//             scrub: 1,
+//         }
+//     })
+//         .to(element, {
+//             stagger: 1,
+//             y: -20,
+//             scrollTrigger: {
+//                 trigger: element,
+//                 start: '-100% 30%',
+//                 end: '50% 35%',
+//                 scrub: 2,
+//                 // markers: true
+//             }
+//         });
+// });
+
+// gsap.utils.toArray('.project-bottom').forEach((element) => {
+//     gsap.timeline({
+//         scrollTrigger: {
+//             trigger: element,
+//             scrub: 1,
+//         }
+//     })
+//         .to(element, {
+//             stagger: 1,
+//             y: -30,
+//             scrollTrigger: {
+//                 trigger: element,
+//                 start: '-75% 30%',
+//                 end: '-15% 35%',
+//                 scrub: 2,
+//                 // markers: true
+//             }
+//         });
+// });
+
+
+
+
+gsap.utils.toArray('.link_button').forEach((element) => {
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: element,
+            scrub: 1,
+        }
+    })
+        .from(element, {
+            stagger: 1,
+            opacity:0,
+            scrollTrigger: {
+                trigger: element,
+                start: '-350px 30%',
+                end: '-10% 35%',
+                scrub: 2,
+                // markers: true
+            }
+        });
+});
+
